d27995b12c91d18303346bd05bed06310fc43d9d
98913 <98913@LAPTOP-JQR66TAF>
2025-05-24 17:57:26 +0330

    Create Resevatin Client

---
+++ ReservationClient/Model/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace ReservationClient.Model;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; } = "";
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Type { get; set; } = "";
+}
---
+++ ReservationClient/Services/ReservationService.cs
@@ -0,0 +1,24 @@
+﻿using ReservationClient.Model;
+
+namespace ReservationClient.Services;
+
+public class ReservationService
+{
+    private readonly HttpClient _http;
+
+    public ReservationService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<List<Reservation>> GetAllAsync()
+    {
+        return await _http.GetFromJsonAsync<List<Reservation>>("api/reservation") ?? new();
+    }
+
+    public async Task<bool> CreateAsync(Reservation reservation)
+    {
+        var response = await _http.PostAsJsonAsync("api/reservation", reservation);
+        return response.IsSuccessStatusCode;
+    }
+}
